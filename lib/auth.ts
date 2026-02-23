3da8f31dddd3cbe2e6c5ad46d1faf5e916dc5cf0
Kevin Chero <vecipremiun@gmail.com>
2026-02-23 01:49:31 -0300

    fix: cast token.role and token.id to string in session callback

--- lib/auth.ts
+++ lib/auth.ts
@@ -75,8 +75,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         },
         async session({ session, token }) {
             if (token) {
-                session.user.role = token.role;
-                session.user.id = token.id;
+                session.user.role = token.role as string;
+                session.user.id = token.id as string;
             }
             return session;
         }
